aaefbdd7c71b155838e37b88fd27e78de383a48f
Andres Gomez <nicolasgomez8873030@gmail.com>
2023-07-27 12:06:14 -0500

    [+] se crearon componentes para header y footer y se actualizo formulario para producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/partial/header/header.component';
+import { FooterComponent } from './component/partial/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

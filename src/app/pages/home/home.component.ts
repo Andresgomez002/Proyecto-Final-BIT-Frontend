d0384276281c318e987bd6aa3a4179064c13dbe7
Andres Gomez <nicolasgomez8873030@gmail.com>
2023-08-02 11:30:59 -0500

    [+] se crearon mas mangas y se completo el html del home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,9 @@ import { ProductsService } from 'src/app/services/products.service';
 export class HomeComponent implements OnInit {
   products: Products = { products: [] };
   productsComedia: Products = { products: []};
+  productsFantasia: Products = { products: []};
+  productsDrama: Products = { products: []};
+  productsFiccion: Products = { products: []};
 
   constructor(
     private productsService: ProductsService
@@ -18,7 +21,13 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.productsService.getNProducts('Accion', 6)
       .subscribe( products => this.products = products );
-      this.productsService.getNProducts('Comedia', 1)
+      this.productsService.getNProducts('Comedia', 6)
       .subscribe( products => this.productsComedia = products );
+      this.productsService.getNProducts('Fantasia', 6)
+      .subscribe( products => this.productsFantasia = products );
+      this.productsService.getNProducts('Drama', 6)
+      .subscribe( products => this.productsDrama = products );
+      this.productsService.getNProducts('Ciencia Ficcion', 6)
+      .subscribe( products => this.productsFiccion = products );
   }
 }
